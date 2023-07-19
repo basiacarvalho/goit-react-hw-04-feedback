52da1f50d0f715a27e6367fce8a9b4c04511c5f5
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 09:12:27 +0200

    refactor of the section component

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,5 +1,5 @@
 import css from './Notification.module.css';
 
-export const Notification = message => {
+export const Notification = ({ message }) => {
   return <p className={css.notification}>{message}</p>;
 };
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
 import css from './Section.module.css';
 
-export class Section extends Component {
-  render() {
-    return (
-      <>
-        <h3 className={css.title}>{this.props.title}</h3>
-        {this.props.children}
-      </>
-    );
-  }
-}
+export const Section = ({ title, children }) => {
+  return (
+    <>
+      <h3 className={css.title}>{title}</h3>
+      {children}
+    </>
+  );
+};
