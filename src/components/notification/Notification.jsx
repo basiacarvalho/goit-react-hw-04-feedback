1e49217780d99aa754f58d8fbcd61929031abfc9
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 09:07:18 +0200

    refactor of the notification component

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
 import css from './Notification.module.css';
 
-export class Notification extends Component {
-  render() {
-    return <p className={css.notification}>{this.props.message}</p>;
-  }
-}
+export const Notification = message => {
+  return <p className={css.notification}>{message}</p>;
+};
