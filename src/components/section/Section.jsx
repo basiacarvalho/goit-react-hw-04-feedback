dc40081371b00e1546a1f2793bc79f30eeb537dc
basiacarvalho <basiacarvalho@gmail.com>
2023-07-31 17:57:53 +0200

    additional fixes

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -10,7 +10,7 @@ export const Section = ({ title, children }) => {
   );
 };
 
-Section.PropTypes = {
+Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
